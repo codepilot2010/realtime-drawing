b5e7baf136d9ddf03acafa3ed046b4572e53ce97
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-05-25 12:27:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+nosex=0;
+nosey=0;
+leftwristx=0;
+rightwristx=0;
+difference=0;
+
 function setup()
 {
     video=createCapture(VIDEO);
@@ -16,12 +22,23 @@ function modelLoaded()
 function draw()
 {
     background('#0cf7ec');
+    document.getElementById("square_size").innerHTML="Width and Height of the Square will be = "+difference+"px";
+    fill('#1703fc');
+    stroke('#03fcad');
+    square(nosex,nosey,difference);
 }
 
 function gotPoses(results)
 {
-    if(results.lenghth>0)
+    if(results.length>0)
     {
         console.log(results);
+        nosex=results[0].pose.nose.x;
+        nosey=results[0].pose.nose.y;
+        console.log("Nose x = "+nosex+"nosey = "+nosey);
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+        console.log("left wrist x = "+leftwristx+"right wrist x = "+rightwristx+"difference is "+difference);
     }
 }
